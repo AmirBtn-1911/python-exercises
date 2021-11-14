160c3de5ef3a420063262a44a5959dc24932ce31
Amir <94225642+AmirBtn-1911@users.noreply.github.com>
2021-11-14 16:58:20 +0330

    Add files via upload

---
+++ test.py
@@ -0,0 +1,73 @@
+# score = 19
+# if score >= 15:
+#     print('you passed')
+# elif score > 20:
+#     print('number is invalid')
+# elif score < 15:
+#     print('you failed')
+
+# numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+#
+# sum = 0
+#
+# for val in numbers:
+#     sum = sum + val
+#
+# print("The sum is", sum)
+
+# i = 0
+# while i < 10:
+#     i += 1
+#     print (i)
+
+# numbers = [6, 5, 3, 8, 4, 2, 5, 4, 11]
+# sum = 0
+# i = 0
+#
+# for val in numbers:
+#     sum = sum + val
+#     i = i + 1
+#     print(i, " : The sum is", sum, 'number ', val)
+
+# numbers = [23,12,3,4,5,2,23,39]
+# i= 0
+# for i in numbers:
+#
+# counter = 0
+#
+# while counter < 3:  # 0 1 2
+#     counter = counter + 1
+#     print(counter)
+#
+# else:
+#     print("Inside else")
+
+# def amir():
+#     x = 1
+#     if x > 1:
+#         print(x * 5)
+#     else:
+#         print(x * 2)
+#
+# amir()
+
+# def number_check(number):
+#     '''this func, check the number'''
+#
+#     if number > 6:
+#         i = 3
+#         while i:
+#             i-=1
+#             print('freezeman')
+#     elif  3>number>0:
+#         print(number)
+#     else:
+#         print('chetory')
+#
+# number = int(input('type a number: '))
+# number_check(number)
+
+def faak(number):
+    '''factorial'''
+
+number = int(input('enter a number :'))
