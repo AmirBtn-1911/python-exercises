20109cbde716ab8773e7b75935ea431fd7880996
Amir <94225642+AmirBtn-1911@users.noreply.github.com>
2021-11-15 06:36:47 -0800

    Update exercise_3.py

--- exercise_3.py
+++ exercise_3.py
@@ -1,8 +1,8 @@
 num_scrs = int(input('enter the number of scores: ')) #3
-avg2 = avg1 = count = 0
-while count < num_scrs: # 5 10 15
-    count += 1
-    avg0 = float(input("enter the %s avg : " %count))
+avg2 = avg1 = counter = 0
+while counter < num_scrs: # 5 10 15
+    counter += 1
+    avg0 = float(input("enter the %s avg : " %counter))
     if avg0 > avg1 :
         avg2 = avg1
         avg1 = avg0
