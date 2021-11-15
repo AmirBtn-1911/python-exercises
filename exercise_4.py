8bd84e903c0455fc367e8aa8e417278ffadd1ef1
Amir <94225642+AmirBtn-1911@users.noreply.github.com>
2021-11-15 09:00:13 -0800

    Update exercise_4.py

--- exercise_4.py
+++ exercise_4.py
@@ -1,8 +1,10 @@
 #first part
+print('First Part')
 n = int(input('enter a number: '))
-print(f"{n, n * '+'}")
+print(n, n * '+'+'\n')
 
 #secend part
+print('Secend Part')
 list = [3,7,16,8,6]
 for index in list:
     print("%2i%s"%(index,index * '+'))
