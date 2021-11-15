373c4e57373232ab4817e095cc3cb96e7224e9b7
Amir <94225642+AmirBtn-1911@users.noreply.github.com>
2021-11-15 08:55:46 -0800

    Create exercise_6.py

---
+++ exercise_6.py
@@ -0,0 +1,26 @@
+#the code bellow is copy!!!
+# def fibonacci(n):
+#     if n<0:
+#         print('incorrect input.')
+#     elif n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fibonacci(n-1) + fibonacci(n-2)
+# print(fibonacci(10))
+
+#this is mine:  (seriously)         0 1 1 2 3 5 8 13 21 ...     fibonacci
+in_num = int(input('enter a number : '))
+num4 = num1 = 0
+num2 = num3 = 1
+while in_num >= num4 :
+    if num3 + num2 >= in_num:
+        break
+    else:
+        num4 = num3 + num2
+        num2 = num3
+        num3 = num4
+        num1 = num2
+    print(num4 , ' ' , end='')
+    continue
