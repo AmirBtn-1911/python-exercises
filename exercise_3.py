8f5a65bda2a9347aab9a08ca27f0e44e154924ef
Amir <94225642+AmirBtn-1911@users.noreply.github.com>
2021-11-16 02:32:36 -0800

    Update exercise_3.py

--- exercise_3.py
+++ exercise_3.py
@@ -1,11 +1,15 @@
-num_scrs = int(input('enter the number of scores: ')) #3
+# this is the exact code the teacher wrote
+num_scrs = int(input('enter the number of scores: '));print()
 avg2 = avg1 = counter = 0
-while counter < num_scrs: # 5 10 15
+while counter < num_scrs:
     counter += 1
-    avg0 = float(input("enter the %s avg : " %counter))
+    code = input('enter the %s student code : '%counter)
+    avg0 = float(input("enter the %s avg : " %counter));print()
+    st_num = {counter : code}
     if avg0 > avg1 :
         avg2 = avg1
         avg1 = avg0
     elif avg0 > avg2 :
         avg2 = avg0
-print(avg2)
+        st_code = code  
+print(avg2,st_code)
