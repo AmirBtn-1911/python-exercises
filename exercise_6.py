e271027867f650b04579700116d7fd9a99a56ff3
Amir <94225642+AmirBtn-1911@users.noreply.github.com>
2021-11-20 11:22:08 -0800

    Update exercise_6.py

--- exercise_6.py
+++ exercise_6.py
@@ -10,20 +10,38 @@
 #         return fibonacci(n-1) + fibonacci(n-2)
 # print(fibonacci(10))
 
-#this is mine:  (seriously)         0 1 1 2 3 5 8 13 21 ...     fibonacci
-in_num = int(input('enter a number : '))
-num4 = num1 = 0
-num2 = num3 = 1
-if in_num > 0 :
-    while in_num >= num4 :
-        if num3 + num2 >= in_num:
-            break
-        else:
-          num4 = num3 + num2
-          num2 = num3
-          num3 = num4
-          num1 = num2
-        print(num4 , ' ' , end='')
-        continue
-else:
-    print('unsopported number!')
+#this is mine:  (seriously)         0 1 1 2 3 5 8 13 21 ...     fibonacci       !!!First attempt
+# in_num = int(input('enter a number : '))
+# num4 = num1 = 0
+# num2 = num3 = 1
+# if in_num > 0 :
+#     while in_num >= num4 :
+#         if num3 + num2 >= in_num:
+#             break
+#         else:
+#           num4 = num3 + num2
+#           num2 = num3
+#           num3 = num4
+#           num1 = num2
+#         print(num4 , ' ' , end='')
+#         continue
+# else:
+#     print('unsopported number!')
+
+#secend attempt
+num3 = 1
+num1 = num2 = 0
+n = int(input('enter a number :'))
+while True:
+    if num3 >= n:     #here
+        break
+    print(num3 , end=' ')
+    # num1 = num2 + num3
+    # num2 = num3
+    # num3 = num1
+
+    #both sides work!!! but you should change the condition(if)
+
+    num1 = num2
+    num2 = num3
+    num3 = num1 + num2
