0967680d5cb7160ef8b6714266098c74a351ddf1
Amir <94225642+AmirBtn-1911@users.noreply.github.com>
2021-12-03 07:27:15 -0800

    Update exercise_11.py

--- exercise_11.py
+++ exercise_11.py
@@ -1,16 +1,21 @@
-text = input('enter tour word : ')
+text = input('enter your word/sentence : ')
 
-A = E = U = I = O = 0
+A = E = U = I = O = 'No result.'
 for i in text:
     if i == 'a' or i == 'A':
+        A = 0
         A += 1
     elif i == 'e' or i == 'E':
+        E = 0
         E += 1
     elif i == 'u' or i == 'U':
+        U = 0
         U += 1
     elif i == 'i' or i == 'I':
+        I = 0
         I += 1
     elif i == 'o' or i == 'O':
+        O = 0
         O += 1
 
 print('letter Aa counts :',A)
