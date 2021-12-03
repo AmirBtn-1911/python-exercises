7a911cc88c641b774fd32fb9fb317e5d6a315c34
Amir <94225642+AmirBtn-1911@users.noreply.github.com>
2021-12-03 08:33:25 -0800

    Update exercise_09.py

--- exercise_09.py
+++ exercise_09.py
@@ -1,15 +1,8 @@
-n = int(input('enter your number : '))
-i = 2
-# for i in range(2,n):
-#     if n % i == 0:
-#         n = int(input('again: '))
-#         i = 2
-while i < n:
-    if n % i == 0:
-        i = 2
-        n = int(input('again'))
-    elif n % i != 0:
-        i += 1
-
-
-#not yet finished
+n = int(input('enter your number : ')); i = 2
+while True:
+    while i < n:
+        if n % i == 0:
+            n = int(input('entered number is not a prime number, enter again: '));continue
+        elif n % i != 0:
+            i += 1;continue
+    print('the number is prime number, goodbye');break
