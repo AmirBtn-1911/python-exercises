32b702c0e5fbcc751d3610580617d7f78b5dd792
Amir <94225642+AmirBtn-1911@users.noreply.github.com>
2021-12-05 18:15:56 +0330

    Update exercise_14.py

--- exercise_14.py
+++ exercise_14.py
@@ -1,8 +1,9 @@
 members = {1:3,2:2,3:4,4:1,5:5,6:3,7:2,8:3}
+all = 0
 cost = int(input('enter the water price: '))
-def operation(m):
-    person = cost / m
-    for i in members:
-        unit = person * members[i]
-        print('unit %s : %.0f tumans'%(i,unit))
-operation(len(members))
+for i in members:
+    all = members[i] + all
+person = cost / all
+for i in members:
+    unit = person * members[i]
+    print('unit %s : %.0f tumans'%(i,unit))
